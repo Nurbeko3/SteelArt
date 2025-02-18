07ccb22717021ef530da08118d028bbb76ab434f
Nurbeko3 <nurkn177@gmail.com>
2025-02-18 16:16:50 +0500

    Oxirgi contact qoldi

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-;(function () {
+(function () {
+  "use strict";
 
-    'use strict';
-
-    var wowAnimation = function() {
-        var wow = new WOW(
-            {
-                animateClass: 'animated',
-                offset:       150,
-                callback:     function(box) {
-                    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-                }
-            }
-        );
-        wow.init();
-    }
-
-
-    (function($) {
-        wowAnimation();
-    })(jQuery);
+  var wowAnimation = (function () {
+    var wow = new WOW({
+      animateClass: "animated",
+      offset: 150,
+      callback: function (box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">");
+      },
+    });
+    wow.init();
+  })(function ($) {
+    wowAnimation();
+  })(jQuery);
+})();
 
 
-}());
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+      let navbar = document.querySelector(".navbar");
+      if (window.scrollY > 50) {
+          navbar.classList.add("fixed-top");
+      } else {
+          navbar.classList.remove("fixed-top");
+      }
+  });
+});
